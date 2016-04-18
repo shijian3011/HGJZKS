a66c46690ea7f445d780e5b9bdb4b67daeeb46a3
john shi <shijian3011@qq.com>
2016-04-18 17:57:54 +0800

    add file

--- HGJZKSOC/HGJZKSOC/AppDelegate/AppDelegate.m
+++ HGJZKSOC/HGJZKSOC/AppDelegate/AppDelegate.m
@@ -9,13 +9,11 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 
-
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
