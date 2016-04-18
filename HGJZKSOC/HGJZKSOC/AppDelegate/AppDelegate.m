84c1b40e97430d2e8488696f7959e9d0032cd62b
john shi <shijian3011@qq.com>
2016-04-18 11:37:12 +0800

    重新组织文件

--- HGJZKSOC/HGJZKSOC/AppDelegate/AppDelegate.m
+++ HGJZKSOC/HGJZKSOC/AppDelegate/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
+
 
 @interface AppDelegate ()
 
@@ -17,6 +19,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor redColor];
+    [self.window makeKeyAndVisible];
+    
+    ViewController *vc = [[ViewController alloc] init];
+    vc.view.backgroundColor = [UIColor grayColor];
+    application.keyWindow.rootViewController = vc;
     return YES;
 }
 
